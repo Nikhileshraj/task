00e135ed56fabbe9389c2925a577428e90ec587c
Nikhileshraj <89075463+Nikhileshraj@users.noreply.github.com>
2021-08-18 12:30:53 +0530

    Add files via upload

---
+++ MirroredRightTrianglePattern.java
@@ -0,0 +1,23 @@
+public class MirroredRightTrianglePattern
+{
+    public static void main(String[] args)
+    {
+        int n=7;
+//inner loop
+        for (int i= 1; i<= n; i++)
+        {
+//outer loop
+            for (int j=1; j<=n-i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k=1;k<=i;k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+
